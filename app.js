db92f5111683c7f8d8ceee7a8df0c38e182dd8dd
Ahmed Adel Abdelrahman <ahmwdadel97@gmail.com>
2023-04-24 13:40:17 +0200

    Show total submissions

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { urlencoded } = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res)=>{
@@ -19,7 +20,7 @@ app.post("/", (req, res)=>{
     var apiKey = "";
     var handle = req.body.userHandle
     console.log(handle);
-    const url = "https://codeforces.com/api/user.info?handles="+handle;
+    const url = "https://codeforces.com/api/user.status?handle="+handle+"&from=1&count=100000000";
     console.log(url);
     https.get(url, (response)=>{
         console.log(response.statusCode);
@@ -30,7 +31,19 @@ app.post("/", (req, res)=>{
         response.on("end", ()=>{
             try {
                 const userData = JSON.parse(data);
-                console.log(userData.result[0].lastName);  
+                //console.log(userData); 
+                const problemsStats =  {total : 0};
+                for(let i = 0; userData.result.length>i; i++){
+                    var current = userData.result[i].verdict;
+                    problemsStats["total"]++;
+                    if (current in problemsStats) problemsStats[current]++;
+                    else  problemsStats[current] = 1;
+                      
+                }
+                console.log(problemsStats);
+                res.render("home", {total: problemsStats["total"]});
+                  
+
             } catch (error) {
                 console.log(`Error parsing JSON: ${error}`);
             }
@@ -41,4 +54,3 @@ app.post("/", (req, res)=>{
 app.listen(3000, (req,res)=>{
     console.log("running on port 3000");
 })
-    //"https://codeforces.com/api/user.status?handle="+name+"&from=1&count=1000000000"
