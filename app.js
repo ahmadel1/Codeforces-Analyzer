856be6d9cb60afd16a5d88f181d47194cb604e96
Ahmed Adel Abdelrahman <ahmwdadel97@gmail.com>
2023-05-03 15:57:27 +0200

    userNotFound message

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const request = require("request");
 const https = require('https');
-const { urlencoded } = require('body-parser');
-
-
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-
 let problemsStats =  {total : 0, OK:0 , WRONG_ANSWER:0, TIME_LIMIT_EXCEEDED: 0, RUNTIME_ERROR: 0, MEMORY_LIMIT_EXCEEDED : 0, other : 0};
-let problemsRatings = []
-let xValues = []
-let yValues = []
+let problemsRatings = [];
+let xValues = [];
+let checked = {};
+let yValues = [];
+let userNotFound = false;
 
 app.get("/", (req, res)=>{
-    res.render("home");
+    res.render("home", {userNotFound:userNotFound});
 })
 
 app.get("/stats" , (req,res)=>{
     if(req.body.userHandle === "") res.redirect("/")
-    const arr1 = ["OK", "WRONG_ANSWER", "TIME_LIMIT_EXCEEDED", "RUNTIME_ERROR", "MEMORY_LIMIT_EXCEEDED", "other"];
-    const arr2 = ["Accepted" , "Wrong Answer", "Time Limit", "Runtime error", "Memory Limit", "Other"];
-    res.render("stats", {data: problemsStats, xValues: xValues, yValues: yValues, arr1 : arr1, arr2 : arr2});
+    const statueValue = ["OK", "WRONG_ANSWER", "TIME_LIMIT_EXCEEDED", "RUNTIME_ERROR", "MEMORY_LIMIT_EXCEEDED", "other"];
+    const statueTitle= ["Accepted" , "Wrong Answer", "Time Limit", "Runtime error", "Memory Limit", "Other"];
+    res.render("stats", {data: problemsStats, xValues: xValues, yValues: yValues, statueValue : statueValue, statueTitle :statueTitle});
 })
 
 app.post("/", (req, res)=>{
     var apiKey = "";
     var handle = req.body.userHandle
-    console.log(handle);
-    handle = removeWhiteSpaces(handle);
-    console.log(handle);
-    const url = "https://codeforces.com/api/user.status?handle="+handle+"&from=1&count=100000000";
-    const inforUrl = "https://codeforces.com/api/user.info?handle="+handle;
+    handle = handle.trim(); //remove white space from begining and ending of the input
 
-    console.log(url);
-
+    const url = "https://codeforces.com/api/user.status?handle="+handle+"&from=1&count=10000000";
+    const infoUrl = "https://codeforces.com/api/user.info?handles="+handle;
+    
     https.get(url, (response)=>{
         console.log(response.statusCode);
         let data = "";
+        userNotFound = false;
 
         response.on("data", (chunk)=>{
             data += chunk;
@@ -50,7 +45,7 @@ app.post("/", (req, res)=>{
         response.on("end", ()=>{
             try {
                 const userData = JSON.parse(data);
-                
+                console.log(userData);
                 //reset variables every new search
                 for (let key in problemsStats) {
                     problemsStats[key] = 0;
@@ -58,16 +53,14 @@ app.post("/", (req, res)=>{
                 problemsRatings = [];
                 xValues = [];
                 yValues = [];
-                console.log(userData.result[0].problem.contestId + userData.result[0].problem.index)
-                let problemID = userData.result[0].problem.contestId + userData.result[0].problem.index;
+                checked = {};
+
                 generateProblemStats(userData);
-                console.log(problemsStats);
-
-                generateProblemDifficulty();
-
+                generateChartData();
                 res.redirect("/stats");
                    
             } catch (error) {
+                userNotFound = true;
                 console.log(`Error parsing JSON: ${error}`);
                 res.redirect("/")
             }
@@ -80,10 +73,7 @@ app.listen(3000, (req,res)=>{
     console.log("running on port 3000");
 })
 
-//remove white space from begining and ending of the input
-function removeWhiteSpaces(handle){
-    return handle.trim();
-}
+
 
 function generateProblemStats(userData){
     for(let i = 0; userData.result.length>i; i++){
@@ -101,16 +91,15 @@ function generateProblemStats(userData){
     }
 }
 
-let checked = {};
 function check(problem){
     if(problem in checked)return false;
     else{
-        checked[problem] = 1;
+        checked[problem] = true;
         return true;
     }
 }
 
-function generateProblemDifficulty(){
+function generateChartData(){
     for(let key in problemsRatings){
         console.log(key + ": " + problemsRatings[key]);
         if(key === "undefined")continue;
